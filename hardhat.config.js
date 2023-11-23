81e5923af2198e51dc5fc278c9f82ecf0434cc02
Lint Action <lint-action@samuelmeuli.com>
2023-11-23 15:34:25 +0000

    Fix code style issues with Prettier

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,39 +6,40 @@ require("hardhat-deploy")
 require("dotenv").config()
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const DEPLOYER_KEY = process.env.DEPLOYER_KEY || "0x0000000000000000000000000000000000000000000000000000000000000001"
+const DEPLOYER_KEY =
+	process.env.DEPLOYER_KEY || "0x0000000000000000000000000000000000000000000000000000000000000001"
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.18",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
-    },
-  },
-  networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-    sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
-      chainId: 11155111,
-      accounts: [DEPLOYER_KEY],
-      confirmations: 2,
-    },
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
-    },
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-  },
+	solidity: {
+		version: "0.8.18",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 1000,
+			},
+		},
+	},
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		sepolia: {
+			url: "https://eth-sepolia.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
+			chainId: 11155111,
+			accounts: [DEPLOYER_KEY],
+			confirmations: 2,
+		},
+	},
+	etherscan: {
+		apiKey: {
+			sepolia: ETHERSCAN_API_KEY,
+		},
+	},
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		},
+	},
 }
