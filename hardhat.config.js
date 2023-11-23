39645dc97fe7a7d0e98988571eec21f9c384a08a
thomga <gabriel.thom02@gmail.com>
2023-11-23 12:33:31 -0300

    chore: update hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,42 +6,39 @@ require("hardhat-deploy")
 require("dotenv").config()
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const DEPLOYER_KEY = process.env.DEPLOYER_KEY
+const DEPLOYER_KEY = process.env.DEPLOYER_KEY || "0x0000000000000000000000000000000000000000000000000000000000000001"
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	vyper: {
-		compilers: [{ version: "0.3.7" }, { version: "0.2.4" }, { version: "0.2.15" }],
-	},
-	solidity: {
-		version: "0.8.18",
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 1000,
-			},
-		},
-	},
-	networks: {
-		hardhat: {
-			chainId: 1337,
-		},
-		goerli: {
-			url: "https://eth-goerli.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
-			chainId: 5,
-			accounts: [DEPLOYER_KEY],
-			confirmations: 2,
-		},
-	},
-	etherscan: {
-		apiKey: {
-			goerli: ETHERSCAN_API_KEY,
-		},
-	},
-	namedAccounts: {
-		deployer: {
-			default: 0,
-		},
-	},
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
+      chainId: 11155111,
+      accounts: [DEPLOYER_KEY],
+      confirmations: 2,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 }
