f30503fdd6c248085843187e5c05b9868c481c19
claudiovb <claudiocarvalhovilasboas@gmail.com>
2023-11-25 22:28:24 +0100

    Add Automated sample contract, sample requests and simulationScript for chainlink scheduled API query

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,15 @@ module.exports = {
 	solidity: {
 		compilers: [
 			{
+				version: "0.8.19",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 1000,
+					},
+				},
+			},
+			{
 				version: "0.8.18",
 				settings: {
 					optimizer: {
